87784098bd8f67e3dd27c34d12d8c7054a34e9a4
ZZANZZAN <40537652+ZZANZZAN@users.noreply.github.com>
2021-01-18 10:36:37 +0300

    Create test1.py

---
+++ SNN(LIFmodel)/test1.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+import redis
+import mnist
+
+from neurons import LIFNeuron as LIF
+from utils import graph_results as graph, image_utils
+
+T         = 20   # total time to simulate (msec) 
+dt        = 0.01 # Simulation timestep
+time      = int(T / dt)
+_T        = 1    # time per simulation step (so if _T is 1 and T is 20, then run T/_T steps)
+
+stride = (4,2)   # TODO: This will be 4 on-centre neurons, 2 surround (off-centre) neurons
+stride_size = stride[0] + stride[1]
+
+debug=True
+
+image, label = image_utils.get_next_image(pick_random = True)  #index=0)
+
+image_utils.graph_retinal_image(image, stride)
+
+neurons = []
+print('Creating {} x {} neurons'.format(len_x, len_y))
+for y in range (0, len_y, 1):
+    neuron_row=[]
+    for x in range(0, len_x, 1):
+        neuron_row.append(LIF.LIFNeuron(neuron_label="{}/{}".format(y,x), debug=debug))
+    neurons.append(neuron_row)
+
+
+start_x = 5
+start_y = 5
+
+print(len_x, len_y)
+
+#for y in range(0, len_y, start_y + stride_size, 1):
+#    for x in range(start_x, start_x + stride_size, 1):
+for y in range(0, len_y, 1):
+    for x in range(0, len_x, 1):
+        stimulus = np.full((time), image_norm[y,x])
+        print('Stimulus from image @ {}/{}: {}'.format(y, x, stimulus))
+        print('Processing spike generator for neuron {}/{}: {}'.format(y,x,neurons[y][x].neuron_label))
+        neurons[y][x].spike_generator(stimulus)
+        print('Stats for neuron {},{}: time={}, stimulus={}, spikes={}' .format(x,y,time, stimulus.shape, neurons[y][x].spikes)) #.shape))
+        #graph.plot_membrane_potential(neurons[neuron_x][neuron_y].time, neurons[neuron_x][neuron_y].Vm, 'Membrane Potential {}'.format(neurons[neuron_x][neuron_y].type), neuron_id = "{}/{}".format(neuron_x, neuron_y))
+        #graph.plot_spikes(neurons[neuron_x][neuron_y].time, neurons[neuron_x][neuron_y].spikes, 'Output spikes for {}'.format(neurons[neuron_x][neuron_y].type), neuron_id = "{}/{}".format(neuron_x, neuron_y))
+
+ny, nx = 5, 21
+print(neurons[ny][nx].time.shape)
+print(neurons[ny][nx].spikes.shape)
+print(neurons[ny][nx].Vm.shape)
+
+print(neurons[ny][nx].neuron_label)
+
+graph.plot_spikes(neurons[ny][nx].time, neurons[ny][nx].spikes, 'Output Spikes for {}'.format(neurons[ny][nx].type), neuron_id = '{}/{}'.format(ny, nx))
+graph.plot_membrane_potential(neurons[ny][nx].time, neurons[ny][nx].Vm, 'Membrane Potential {}'.format(neurons[ny][nx].type), neuron_id = '{}/{}'.format(ny, nx))
+
+l2_neurons_x = int(len_x / stride[0])
+l2_neurons_y = int(len_y / stride[0])
+print ('Creating layer 2 neurons: {} by {}'.format(l2_neurons_y, l2_neurons_x))
+
+l2_neurons = []
+
+for y in range(l2_neurons_y):
+    l2_neuron_row = []
+    for x in range(l2_neurons_x):
+        l2_neuron_row.append(LIF.LIFNeuron(neuron_label="L2:{}/{}".format(y,x), debug=debug))
+    l2_neurons.append(l2_neuron_row)
+
+l2_neuron_stimulus = np.zeros((l2_neurons_y, l2_neurons_x, time))
+
+mult_factor = 49           # Number of neurons for each pixel (this is a hack to reduce processing earlier,... 
+                          # raise to 49 perhaps later!)
+l2x, l2y = 0,0
+for ry in range (0, len_y, stride[0]):
+    l2x = 0
+    for rx in range(0, len_x, stride[0]):
+        #x_end = rx + stride[0]
+        #y_end = ry + stride[0]       
+        stimulus_ret_unit = np.zeros(time)
+        print('Generating stimulus for L2 neurons {}/{}'.format(l2y, l2x))
+        for ny in range(stride[0]):
+            for nx in range(stride[0]):
+                x = rx + nx
+                y = ry + ny
+                #print ('Processing stimulus for L1 neuron at {},{}'.format(y,x))
+                #print ('Adding stimulus {}'.format(neurons[y][x].spikes[:time]))
+                stimulus_ret_unit += neurons[y][x].spikes[:time] * mult_factor
+        print ('Adding stimulus for l2 neuron {},{}, duration={}\n'.format(l2y, l2x, len(stimulus_ret_unit)))
+        l2_neuron_stimulus[l2y,l2x,:] = stimulus_ret_unit
+        l2x += 1
+    l2y += 1
+
+for y in range(l2_neurons_y):
+    for x in range(l2_neurons_x):
+        print('\nProcessing L2 Neuron {},{} with stimulus length {}'.format(y,x,l2_neuron_stimulus[y][x]))
+        l2_neurons[y][x].spike_generator(l2_neuron_stimulus[y][x])
+
+#fig, axes = plt.subplots(ncols=l2_neurons_x, nrows=l2_neurons_y, figsize=(10,10))
+fig=plt.figure(figsize=(12,12))
+fig.set_tight_layout({5,2,5})
+
+for y in range(l2_neurons_y):
+    for x in range(l2_neurons_x):
+        index=(y)*l2_neurons_x+x+1
+        id = "{}{}{}".format(l2_neurons_y, l2_neurons_x,index)
+        a=fig.add_subplot(l2_neurons_y, l2_neurons_x, index)
+        a.plot(l2_neurons[y][x].time, l2_neurons[y][x].Vm)
+        a.axis('off')
+        plt.title('Vm at {},{}'.format(y,x))
+        
+plt.show()
+
+#fig, axes = plt.subplots(ncols=l2_neurons_x, nrows=l2_neurons_y, figsize=(10,10))
+fig=plt.figure(figsize=(12,12))
+fig.set_tight_layout({5,2,5})
+
+for y in range(l2_neurons_y):
+    for x in range(l2_neurons_x):
+        index=(y)*l2_neurons_x+x+1
+        id = "{}{}{}".format(l2_neurons_y, l2_neurons_x,index)
+        a=fig.add_subplot(l2_neurons_y, l2_neurons_x, index)
+        a.plot(l2_neurons[y][x].time, l2_neurons[y][x].spikes)
+        a.axis('off')
+        plt.title('Spikes at {},{}'.format(y,x))
+        
+plt.show()
+
