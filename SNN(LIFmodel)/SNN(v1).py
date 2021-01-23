1a87655a8352c29babdd0195fa41f31ebc8de146
ZZANZZAN <40537652+ZZANZZAN@users.noreply.github.com>
2021-01-23 21:34:40 +0300

    Update SNN(v1).py

--- SNN(LIFmodel)/SNN(v1).py
+++ SNN(LIFmodel)/SNN(v1).py
@@ -16,9 +16,7 @@ stride = (4,2)
 stride_size = stride[0] + stride[1]
 kernel_size = 5
 debug=False
-
 #image, label = image_utils.get_next_image(pick_random = True)  
-
 #image_utils.graph_retinal_image(image, stride)
 
 len_x = 28
@@ -35,8 +33,8 @@ for y in range (0, len_y, 1):
 
 for y in range(0, len_y, 1):
     for x in range(0, len_x, 1):
-        stimulus = np.full((time), image[y,x])
-        neurons_l1[y][x].spike_generator(stimulus)
+        neuron_l1_stimulus = np.full((time), image[y,x])
+        neurons_l1[y][x].spike_generator(neuron_l1_stimulus)
 
 # Инициализация второго (сверточного) слоя
 conv_kernel_layer2_1 = np.random.random((kernel_size,kernel_size))
@@ -62,3 +60,24 @@ for y in range (0, len_y_l2, 1):
     for x in range(0, len_x_l2, 1):
         neuron_row.append(LIF.LIFNeuron(neuron_label="L2:{}/{}".format(y,x), debug=debug))
     neurons_l2.append(neuron_row)
+
+neuron_l2_stimulus = np.zeros((len_x_l2, len_y_l2, time))
+mult_factor = 50
+
+l2x, l2y = 0,0
+for x1 in range(0, len_x_l2, 1):
+	l2y = 0
+    for y1 in range(0, len_y_l2, 1):
+    	stimulus_ret_unit = np.zeros(time)
+    	for x2 in range(kernel_size):
+            for y2 in range(kernel_size):
+            	x = x1+x2
+            	y = y1+y2
+            	stimulus_ret_unit += neurons[x][y].spikes[:time] * mult_factor * conv_kernel_layer2_1[x2][y2]
+        l2_neuron_stimulus[l2x,l2y,:] = stimulus_ret_unit
+        l2y += 1
+    l2x += 1
+
+for x in range(len_x_l2):
+    for y in range(len_y_l2):
+        neurons_l2[x][y].spike_generator(l2_neuron_stimulus[x][y])
