24480e3313b28122fba5d896fb65d9599a095a9c
ZZANZZAN <40537652+ZZANZZAN@users.noreply.github.com>
2021-01-31 18:53:52 +0300

    Add files via upload

---
+++ SNN(LIFmodel)/utils/creating_weights.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+
+def cr_W(kernel_size, num_feature_maps, num_full_con_lay):
+	conv_kernel_layer2 = (0.5 - (-0.5))*np.random.random((kernel_size, kernel_size, num_feature_maps)) - 0.5
+	full_con_lay_W = (0.5 - (-0.5))*np.random.random((num_full_con_lay)) - 0.5
+	full_out_lay_W = (0.5 - (-0.5))*np.random.random((num_full_con_lay, num_feature_maps)) - 0.5
+	pool_kernel_l3 = np.array([[1,1],[1,1]])
+
+	np.save('data_weight/conv_kernel_layer2',conv_kernel_layer2)
+	np.save('data_weight/full_con_lay_W',full_con_lay_W)
+	np.save('data_weight/full_out_lay_W',full_out_lay_W)
+	np.save('data_weight/pool_kernel_l3',pool_kernel_l3)
