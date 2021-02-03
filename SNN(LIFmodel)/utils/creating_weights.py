6cb15e6683c1d11fe0f1fef4b24062fca6467fc3
ZZANZZAN <40537652+ZZANZZAN@users.noreply.github.com>
2021-02-03 14:33:03 +0300

    Update creating_weights.py

--- SNN(LIFmodel)/utils/creating_weights.py
+++ SNN(LIFmodel)/utils/creating_weights.py
@@ -4,11 +4,11 @@ import pandas as pd
 import random
 
 def cr_W(kernel_size, num_feature_maps, num_full_con_lay):
-	conv_kernel_layer2 = (0.5 - (-0.5))*np.random.random((kernel_size, kernel_size, num_feature_maps)) - 0.5
+	conv_kernel_layer2 = (0.5 - (-0.3))*np.random.random((kernel_size, kernel_size, num_feature_maps)) - 0.3
 	conv_kernel_layer2.round(1)
-	full_con_lay_W = (0.5 - (-0.5))*np.random.random((num_full_con_lay)) - 0.5
+	full_con_lay_W = (0.5 - (-0.3))*np.random.random((num_full_con_lay)) - 0.3
 	full_con_lay_W.round(1)
-	full_out_lay_W = (0.5 - (-0.5))*np.random.random((num_full_con_lay, num_feature_maps)) - 0.5
+	full_out_lay_W = (0.5 - (-0.3))*np.random.random((num_full_con_lay, num_feature_maps)) - 0.3
 	full_out_lay_W.round(1)
 	pool_kernel_l3 = np.array([[1,1],[1,1]])
 	pool_kernel_l3.round(1)
